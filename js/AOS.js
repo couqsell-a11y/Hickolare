1bef2ae2b7327648f3b5caabfd2ab7ce916954dd
couqsell-a11y <couqsell@gmail.com>
2025-11-03 21:08:30 +0100

    mobile fix test #2

--- js/AOS.js
+++ js/AOS.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
   AOS.init({
-    duration: 650, // 1 second fade duration
+    duration: 700, // 1 second fade duration
     once: true,     // animations happen only once
-    offset: 60,    // start animation slightly before in view
+    offset: 100,    // start animation slightly before in view
     easing: 'ease-in-out'
   });
 });
